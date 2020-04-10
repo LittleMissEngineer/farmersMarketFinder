04117737e47b84ed79150632103b92ae3fbe6a28
Melanie Moore <littlemsengineer@gmail.com>
2020-04-10 18:51:23 -0400

    Added the rest of the data

--- js/index.js
+++ js/index.js
@@ -116,9 +116,7 @@ function createMarker(latlng, name, address, openStatusText, socialMedia,postal,
                 <div class="circle">
                     <i class="fas fa-location-arrow"></i>
                 </div>
-                <span>${address}
-                <a href = "https://www.google.com/maps/dir/?api=1&query=latlng"></a>
-                </span><br>
+                <span src = "https://www.google.com/maps/dir/?api=1&query=latlng">${address}</span><br>
                 <span>${postal}</span>
             </div>
             <div class="store-info-phone">
