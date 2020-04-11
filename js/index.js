967c10060dfc91acae96b17ad6ded284c1933b31
Melanie Moore <littlemsengineer@gmail.com>
2020-04-11 01:40:36 -0400

    Finally figured out link situation

--- js/index.js
+++ js/index.js
@@ -123,7 +123,7 @@ function createMarker(latlng, name, address, openStatusText, socialMedia,postal,
                 <div class="circle">
                     <i class="fas fa-phone-alt"></i>
                 </div>
-               <a href = ${socialMedia}></a>
+               <a href = ${socialMedia}> ${socialMedia}</a>
             </div>
         </div>
     `;
